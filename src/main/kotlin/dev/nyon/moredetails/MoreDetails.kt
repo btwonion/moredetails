e7c3f5cd7872f0217c6f2a0493408a9299700206
Anton <anton.wiblishauser@gmx.de>
2022-11-17 17:23:52 +0100

    register components on startup

--- src/main/kotlin/dev/nyon/moredetails/MoreDetails.kt
+++ src/main/kotlin/dev/nyon/moredetails/MoreDetails.kt
@@ -1,15 +1,21 @@
 package dev.nyon.moredetails
 
+import dev.nyon.moredetails.config.config
 import dev.nyon.moredetails.config.loadConfig
 import kotlinx.serialization.json.Json
 
 val json = Json {
     prettyPrint = true
+    encodeDefaults = true
 }
 
 object MoreDetails {
 
     fun init() {
         loadConfig()
+
+        config.components.filter { it.enabled }.forEach {
+            it.register()
+        }
     }
 }
