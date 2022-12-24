19095ec3f1028c3d3a1398671fbd5b4796bdc4af
Anton <anton.wiblishauser@gmx.de>
2022-12-24 20:45:05 +0100

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
-    kotlin("plugin.serialization") version "1.7.21"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.serialization") version "1.7.22"
     id("fabric-loom") version "1.0-SNAPSHOT"
-    id("io.github.juuxel.loom-quiltflower") version "1.7.4"
-    id("org.quiltmc.quilt-mappings-on-loom") version "4.2.1"
+    id("io.github.juuxel.loom-quiltflower") version "1.8.0"
+    id("org.quiltmc.quilt-mappings-on-loom") version "4.2.3"
 
-    id("com.modrinth.minotaur") version "2.4.4"
+    id("com.modrinth.minotaur") version "2.5.0"
     id("com.github.breadmoirai.github-release") version "2.4.1"
     `maven-publish`
     signing
@@ -16,7 +16,7 @@ plugins {
 
 group = "dev.nyon"
 val majorVersion = "1.0.0"
-version = "$majorVersion-1.19.2-alpha4"
+version = "$majorVersion-1.19.3-alpha5"
 description = "Adds useful information to your ingame hud"
 val authors = listOf("btwonion")
 val githubRepo = "btwonion/moredetails"
@@ -26,14 +26,14 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.2")
+    minecraft("com.mojang:minecraft:1.19.3")
     mappings(loom.layered {
-        //addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:1.19.2+build.21:v2"))
+        //addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:1.19.3+build.8:v2"))
         officialMojangMappings()
     })
-    modImplementation("net.fabricmc:fabric-loader:0.14.10")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.66.0+1.19.2")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.8.6+kotlin.1.7.21")
+    modImplementation("net.fabricmc:fabric-loader:0.14.12")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.70.0+1.19.3")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.8.7+kotlin.1.7.22")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     /*
@@ -95,7 +95,7 @@ modrinth {
     versionNumber.set("${project.version}")
     versionType.set("alpha")
     uploadFile.set(tasks["remapJar"])
-    gameVersions.set(listOf("1.19.2"))
+    gameVersions.set(listOf("1.19.3"))
     loaders.set(listOf("fabric", "quilt"))
     dependencies {
         required.project("fabric-api")
