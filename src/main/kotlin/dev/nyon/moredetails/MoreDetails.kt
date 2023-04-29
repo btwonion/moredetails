0c1484a2df2038421f382fe9928610350fdb552b
Anton <anton.wiblishauser@gmx.de>
2023-04-29 22:29:57 +0200

    create keybinds

--- src/main/kotlin/dev/nyon/moredetails/MoreDetails.kt
+++ src/main/kotlin/dev/nyon/moredetails/MoreDetails.kt
@@ -1,8 +1,12 @@
 package dev.nyon.moredetails
 
+import com.mojang.blaze3d.platform.InputConstants
 import dev.nyon.moredetails.config.config
 import dev.nyon.moredetails.config.loadConfig
 import kotlinx.serialization.json.Json
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
+import net.minecraft.client.KeyMapping
+import org.lwjgl.glfw.GLFW
 
 val json = Json {
     prettyPrint = true
@@ -10,6 +14,24 @@ val json = Json {
 }
 
 object MoreDetails {
+    val configKeyBind = KeyBindingHelper.registerKeyBinding(
+        KeyMapping(
+            "Open MoreDetails config screen",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_U,
+            "moredetails"
+        )
+    )
+
+    val reorderKeyBind = KeyBindingHelper.registerKeyBinding(
+        KeyMapping(
+            "Reorder MoreDetails components",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_I,
+            "moredetails"
+        )
+    )
+
     fun init() {
         loadConfig()
 
