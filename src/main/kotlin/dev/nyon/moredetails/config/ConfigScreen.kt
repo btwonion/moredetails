131d0c7ef74ca70d4924cef60d1889a18251c40c
Anton <anton.wiblishauser@gmx.de>
2023-04-30 18:51:55 +0200

    add accessibility options

--- src/main/kotlin/dev/nyon/moredetails/config/ConfigScreen.kt
+++ src/main/kotlin/dev/nyon/moredetails/config/ConfigScreen.kt
@@ -1,8 +1,10 @@
 package dev.nyon.moredetails.config
 
-import dev.isxander.yacl.api.ConfigCategory
-import dev.isxander.yacl.api.OptionGroup
-import dev.isxander.yacl.api.YetAnotherConfigLib
+import dev.isxander.yacl.api.*
+import dev.isxander.yacl.gui.controllers.ActionController
+import dev.isxander.yacl.gui.controllers.TickBoxController
+import dev.nyon.moredetails.components.reorder.ReorderScreen
+import dev.nyon.moredetails.minecraft
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.Component
 
@@ -10,6 +12,7 @@ fun generateConfigScreen(parent: Screen?): Screen {
     val configScreenBuilder = YetAnotherConfigLib.createBuilder()
     configScreenBuilder.title(Component.literal("moredetails"))
     configScreenBuilder.appendComponentsCategory()
+    configScreenBuilder.appendAccessibilityCategory()
     configScreenBuilder.save {
         config.components.forEach {
             if (it.enabled) it.register() else it.remove()
@@ -24,7 +27,33 @@ fun YetAnotherConfigLib.Builder.appendComponentsCategory(): YetAnotherConfigLib.
     this.category(
         ConfigCategory.createBuilder().name(Component.literal("Components"))
             .tooltip(Component.literal("Manage all of your components in here"))
+            .option(
+                ButtonOption.createBuilder()
+                    .name(Component.literal("Reorder components"))
+                    .tooltip(Component.literal("Opens a screen to reorder the components."))
+                    .action { screen, _ ->
+                        minecraft.setScreen(ReorderScreen(null))
+                        screen.config.saveFunction()
+                    }
+                    .available(minecraft.currentServer != null || minecraft.isSingleplayer)
+                    .controller(::ActionController)
+                    .build()
+            )
             .groups(config.components.map { it.createYACLGroup(OptionGroup.createBuilder()) }).build()
+    )
+    return this
+}
+
+fun YetAnotherConfigLib.Builder.appendAccessibilityCategory(): YetAnotherConfigLib.Builder {
+    this.category(
+        ConfigCategory.createBuilder().name(Component.literal("Accessibility"))
+            .tooltip(Component.literal("Change accessibility settings"))
+            .option(
+                Option.createBuilder(Boolean::class.java).name(Component.literal("Text shadow"))
+                    .tooltip(Component.literal("Decides whether the text should have a shadow or not."))
+                    .binding(config.textShadow, { config.textShadow }, { new -> config.textShadow = new })
+                    .controller(::TickBoxController).build()
+            ).build()
     )
     return this
 }
